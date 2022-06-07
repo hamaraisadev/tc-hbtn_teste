c4d4a6188da622c1aabd2a8a8ee77421c2f89169
CIANDT\hamaraisa <hamaraisa@ciandt.com>
2022-06-06 21:57:05 -0300

    Introdução à Testes de software

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,14 @@
+package email;
+
+public class Pessoa {
+
+
+    public static boolean emailValid(String emal) {
+        if(emal.contains("@") && emal.length() <= 50 ) {
+            return true;
+        }
+        return false;
+
+    }
+}
+
